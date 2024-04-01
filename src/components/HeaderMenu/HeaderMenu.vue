e52dad895bd2fa87bd9e2bbf9306b7fcb0c634c3
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-01 13:28:46 +0300

    feat(custom): add header menu component

---
+++ src/components/HeaderMenu/HeaderMenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="headerMenu"></nav>
+</template>
+
+<script>
+  import HeaderMenu from "./HeaderMenu.model";
+
+  export default HeaderMenu;
+</script>
+
+<style lang="scss" scoped>
+  @import "./HeaderMenu.module";
+</style>
