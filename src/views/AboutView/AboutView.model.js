40b36cd26dc20ab8a3ff7279ab67113205f08f9c
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-01 14:12:45 +0300

    feat(custom): added change header state
    Доабвил изменения состояния хэдера при переключениями между страницами

--- src/views/AboutView/AboutView.model.js
+++ src/views/AboutView/AboutView.model.js
@@ -1,7 +1,17 @@
+import { mapActions } from "vuex";
+
 import MainHeader from "@/components/MainHeader/MainHeader.vue";
 
 export default {
   components: {
     MainHeader,
+  },
+  methods: {
+    ...mapActions({
+      setHeaderDark: "header/setHeaderDark",
+    }),
+  },
+  created() {
+    this.setHeaderDark();
   },
 };
--- src/views/HomeView/HomeView.model.js
+++ src/views/HomeView/HomeView.model.js
@@ -1,7 +1,17 @@
+import { mapActions } from "vuex";
+
 import MainHeader from "@/components/MainHeader/MainHeader.vue";
 
 export default {
   components: {
     MainHeader,
+  },
+  methods: {
+    ...mapActions({
+      setHeaderLight: "header/setHeaderLight",
+    }),
+  },
+  created() {
+    this.setHeaderLight();
   },
 };
