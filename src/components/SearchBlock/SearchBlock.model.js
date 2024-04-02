caae401668c9c795b3cf5ff0239fb49e57b875f5
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-02 11:24:22 +0300

    feat(custom): change images
    Убрал загрузку изображений, так как она не работает, добавил изображения в переменные

--- src/components/SearchBlock/SearchBlock.model.js
+++ src/components/SearchBlock/SearchBlock.model.js
@@ -4,11 +4,4 @@ export default {
   components: {
     SearchActions,
   },
-  created() {
-    const imageUrls = ["/images/searchBg.png", "/images/searchBg-min.png"];
-    imageUrls.forEach((url) => {
-      const img = new Image();
-      img.src = url;
-    });
-  },
 };
