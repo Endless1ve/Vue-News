681e7e81bc9ae073ac7081179f74f5db1b0a11c1
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-08 15:37:07 +0300

    feat(custom): added menu closing when route
    Добавил закрытие меню за мобильных разрешениях при переходе по ссылке

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
-import HomeView from "@/views/HomeView/HomeView.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
+import store from "@/store";
+
+import HomeView from "@/views/HomeView/HomeView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
@@ -9,9 +11,15 @@ const routes = [
     component: () => import("@/views/AboutView/AboutView.vue"),
   },
 ];
+
 const router = createRouter({
   history: createWebHashHistory("/Vue-News/"),
   routes,
+});
+
+router.beforeEach((from, to, next) => {
+  store.commit("menu/closeMenu");
+  next();
 });
 
 export default router;
