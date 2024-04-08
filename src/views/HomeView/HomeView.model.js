d979c9449dec4c71050c1030e04d4152a8ce97fa
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-08 13:33:56 +0300

    feat(custom): added no found block

---
+++ src/views/HomeView/HomeView.model.js
@@ -0,0 +1,7 @@
+import NoFound from "@/components/UI/NoFound/NoFound.vue";
+
+export default {
+  components: {
+    NoFound,
+  },
+};
--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>home</div>
+  <div><NoFound></NoFound></div>
 </template>
 
 <script>
